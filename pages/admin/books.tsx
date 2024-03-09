d1122b26d42ba0f5c068e1eca173ef8f495dba49
RoulaHwaije <153372642+RoulaHwaije@users.noreply.github.com>
2024-03-09 18:25:32 +0100

    Create books.tsx

---
+++ pages/admin/books.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import axios from "axios";
+import BookList from "../../components/BookList";
+import style from "../../styles/Home.module.css";
+import { useRouter } from "next/router";
+import { MdAddToPhotos } from "react-icons/md";
+
+interface Book {
+  _id: string;
+  uniquekey: string;
+  title: string;
+  author: string;
+  coverImage: string;
+  description: string;
+  price: string;
+}
+
+const Books: React.FC<{ books: Book[] }> = ({ books }) => {
+  const router = useRouter();
+
+  const handleClick: React.MouseEventHandler<HTMLButtonElement> = () => {
+    router.push("/admin/books/addnew");
+  };
+
+  return (
+    <div className={style.maincontainer}>
+      <div className={style.section1}>
+        <img
+          src="http://localhost:3000/bookadmin.jpg"
+          alt="book2"
+          className={style.imageAdmin}
+        />
+        <div className={style.textsection}>
+          <h1 className={style.line1}>Easily manage, update, and view</h1>
+          <h1 className={style.line2}>all books details at our shop center</h1>
+          <div className={style.btnsection}>
+            {/* <h2>Add a new Book </h2> */}
+            <button className={style.addbtn2} onClick={handleClick}>
+              Add a new Book
+            </button>
+          </div>
+        </div>
+      </div>
+      <BookList books={books} isAdmin={true} />
+    </div>
+  );
+};
+
+export async function getServerSideProps() {
+  const res = await axios.get(
+    "https://company-78c40.europe-west1.firebasedatabase.app/books.json"
+  );
+  const DATA = res.data;
+  console.log("data : ", DATA); // -> object
+  var books: any[] = [];
+
+  for (let key in DATA) {
+    DATA[key].uniquekey = key;
+    books.push(DATA[key]);
+  }
+  console.log(" -- ", books);
+  return { props: { books } };
+}
+
+export default Books;
