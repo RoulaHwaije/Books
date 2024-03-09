720ad72e48c60c3f69cae00ab7b53c0244bf738e
RoulaHwaije <153372642+RoulaHwaije@users.noreply.github.com>
2024-03-09 18:26:44 +0100

    Create [id].tsx

---
+++ pages/admin/books/[id].tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import BookDetail from "../../../components/BookDetail";
+import Modal from "../../../components/Modal";
+
+const BookDetailPage: React.FC<{ book: any }> = ({ book }) => {
+  const router = useRouter();
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(true);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => {
+    router.push("/admin/books");
+    setIsModalOpen(false);
+  };
+
+  return (
+    <Modal isOpen={isModalOpen} onClose={closeModal}>
+      {/* <h1>Book Details</h1> */}
+      <BookDetail book={book} onClose={closeModal} />
+    </Modal>
+  );
+};
+
+export async function getServerSideProps(context: any) {
+  const { params } = context;
+  const bookId = params.id;
+  const res = await axios.get(
+    `https://company-78c40.europe-west1.firebasedatabase.app/books/${bookId}.json`
+  );
+  const book = res.data;
+  return { props: { book } };
+}
+
+export default BookDetailPage;
